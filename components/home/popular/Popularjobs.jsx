90b0b484a2656839e5880d6edc95f08d483fba90
Muhammad Popoola <popoolama@yahoo.com>
2023-08-05 18:21:47 +0100

    bug fixes

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -18,7 +18,8 @@ const Popularjobs = () => {
   const [selectedJob, setSelectedJob] = useState();
 
   const handleCardPress = (item) => {
-    
+    router.push(`/job-details/${item.job_id}`)
+    setSelectedJob(item.job_id)
   }
  
   return (
@@ -40,6 +41,8 @@ const Popularjobs = () => {
             renderItem={({item}) => (
               <PopularJobCard 
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
             keyExtractor={item => item?.job_id}
